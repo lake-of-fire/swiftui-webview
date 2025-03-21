d80b7eb5e6d98cd637ccebf557ea339f92fb4ac4
Alex Ehlke <alex.ehlke@gmail.com>
2025-03-21 02:30:05 -0400

    wip

--- Sources/SwiftUIWebView/WebViewCoordinator+UIScrollViewDelegate.swift
+++ Sources/SwiftUIWebView/WebViewCoordinator+UIScrollViewDelegate.swift
@@ -1,20 +1,25 @@
 #if os(iOS)
+import SwiftUI
 import UIKit
 import WebKit
 
 extension WebViewCoordinator: UIScrollViewDelegate {
     public func scrollViewDidScroll(_ scrollView: UIScrollView) {
         guard scrollView.isTracking || scrollView.isDragging else { return }
-
+        
         let currentOffset = scrollView.contentOffset.y
-        let threshold: CGFloat = 10.0
+        let threshold: CGFloat = accumulatedScrollOffset > 0 ? 50.0 : 10.0
         let scrollDifference = currentOffset - lastContentOffset
         accumulatedScrollOffset += scrollDifference
         
         if abs(accumulatedScrollOffset) >= threshold {
             let newValue = accumulatedScrollOffset > 0
             if newValue != hideNavigationDueToScroll.wrappedValue {
-                hideNavigationDueToScroll.wrappedValue = newValue
+                DispatchQueue.main.async {
+                    withAnimation(.easeIn(duration: newValue ? 0.3 : 0.1)) {
+                        self.hideNavigationDueToScroll.wrappedValue = newValue
+                    }
+                }
             }
             accumulatedScrollOffset = 0 // Reset after state change
         }
